c660cf1df47fb239c0a4ca8f17cc707eeeb4145d
Noll, Jan (TUL) <ext.Jan.Noll@skoda-auto.cz>
2019-03-22 23:38:17 +0100

    Nacrtnuti generatoru dat

---
+++ RDB.DataCreator/Generators/BasicGenerator.cs
@@ -0,0 +1,92 @@
+﻿using RDB.Data.DAL;
+using RDB.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDB.DataCreator.Generators
+{
+    public class BasicGenerator
+    {
+        #region Fields
+
+        private readonly DefaultContext defaultContext;
+
+        #endregion
+
+        #region Constructors
+
+        public BasicGenerator(DefaultContext defaultContext)
+        {
+            this.defaultContext = defaultContext;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public void Generate()
+        {
+            List<String> BusPlates = GenerateBuses();
+
+            //List<String> Places = 
+
+            defaultContext.SaveChanges();
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void GenerateModels()
+        {
+            defaultContext.Models.AddRange(new Model[]
+            {
+                new Model
+                {
+                    Name= "Karosa"
+                },
+                new Model
+                {
+                    Name = "Iveco"
+                },
+                new Model
+                {
+                    Name= "Volvo"
+                }
+            });
+        }
+
+        private List<String> GenerateBuses()
+        {
+            GenerateModels();
+
+            List<Bus> buses = new List<Bus>
+            {
+                new Bus
+                {
+                    Plate = "123 4567",
+                    ModelName ="Karosa"
+                },
+                new Bus
+                {
+                    Plate = "4LC 4732",
+                    ModelName = "Iveco"
+                },
+                new Bus
+                {
+                    Plate = "1AC 7D4C1",
+                    ModelName = "Volvo"
+                }
+            };
+
+            defaultContext.Buses.AddRange(buses);
+
+            return buses.Select(b => b.Plate).ToList();
+        }
+
+        #endregion
+    }
+}
---
+++ RDB.DataCreator/Program.cs
@@ -0,0 +1,24 @@
+﻿using RDB.Data.DAL;
+using RDB.DataCreator.Generators;
+using System;
+
+namespace RDB.DataCreator
+{
+    class Program
+    {
+        #region Init method
+
+        static void Main(string[] args)
+        {
+            DefaultContext defaultContext = new DefaultContext();
+
+            // Generate test data
+            new BasicGenerator(defaultContext).Generate();
+
+            Console.WriteLine(" --- DATA BYLA USPESNE GENEROVANA ---");
+            Console.ReadKey();
+        }
+
+        #endregion
+    }
+}

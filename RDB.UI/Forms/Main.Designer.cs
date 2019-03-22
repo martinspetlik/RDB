c340798d865158514a12a42cd1d477d975cbe41e
Vojta Hájek <vojtech.hajek1@tul.cz>
2019-03-22 14:15:45 +0100

    Já to chci jenom nějak nahrát na Git :(

--- RDB.UI/Forms/Main.Designer.cs
+++ RDB.UI/Forms/Main.Designer.cs
@@ -28,23 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabs = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tables_cb = new System.Windows.Forms.ComboBox();
+            this.insert_bt = new System.Windows.Forms.Button();
+            this.preview = new System.Windows.Forms.ListView();
+            this.soubor_in_bt = new System.Windows.Forms.Button();
+            this.cesta_in_tb = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.all_tables_ch = new System.Windows.Forms.CheckBox();
+            this.tabs.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabs
+            // 
+            this.tabs.Controls.Add(this.tabPage1);
+            this.tabs.Controls.Add(this.tabPage2);
+            this.tabs.Location = new System.Drawing.Point(12, 12);
+            this.tabs.Name = "tabs";
+            this.tabs.SelectedIndex = 0;
+            this.tabs.Size = new System.Drawing.Size(580, 335);
+            this.tabs.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.all_tables_ch);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.tables_cb);
+            this.tabPage1.Controls.Add(this.insert_bt);
+            this.tabPage1.Controls.Add(this.preview);
+            this.tabPage1.Controls.Add(this.soubor_in_bt);
+            this.tabPage1.Controls.Add(this.cesta_in_tb);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(572, 309);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Import";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(572, 484);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Export";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tables_cb
+            // 
+            this.tables_cb.FormattingEnabled = true;
+            this.tables_cb.Location = new System.Drawing.Point(14, 81);
+            this.tables_cb.Name = "tables_cb";
+            this.tables_cb.Size = new System.Drawing.Size(130, 21);
+            this.tables_cb.TabIndex = 22;
+            // 
+            // insert_bt
+            // 
+            this.insert_bt.Location = new System.Drawing.Point(465, 256);
+            this.insert_bt.Name = "insert_bt";
+            this.insert_bt.Size = new System.Drawing.Size(83, 44);
+            this.insert_bt.TabIndex = 21;
+            this.insert_bt.Text = "Vložit";
+            this.insert_bt.UseVisualStyleBackColor = true;
+            this.insert_bt.Click += new System.EventHandler(this.insert_bt_Click);
+            // 
+            // preview
+            // 
+            this.preview.Location = new System.Drawing.Point(161, 42);
+            this.preview.Name = "preview";
+            this.preview.Size = new System.Drawing.Size(387, 208);
+            this.preview.TabIndex = 20;
+            this.preview.UseCompatibleStateImageBehavior = false;
+            // 
+            // soubor_in_bt
+            // 
+            this.soubor_in_bt.Location = new System.Drawing.Point(465, 6);
+            this.soubor_in_bt.Name = "soubor_in_bt";
+            this.soubor_in_bt.Size = new System.Drawing.Size(83, 31);
+            this.soubor_in_bt.TabIndex = 19;
+            this.soubor_in_bt.Text = "Vybrat soubor";
+            this.soubor_in_bt.UseVisualStyleBackColor = true;
+            this.soubor_in_bt.Click += new System.EventHandler(this.soubor_in_bt_Click);
+            // 
+            // cesta_in_tb
+            // 
+            this.cesta_in_tb.Location = new System.Drawing.Point(161, 11);
+            this.cesta_in_tb.Name = "cesta_in_tb";
+            this.cesta_in_tb.Size = new System.Drawing.Size(298, 20);
+            this.cesta_in_tb.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Cesta k souboru pro import";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Rozložení dat:";
+            // 
+            // all_tables_ch
+            // 
+            this.all_tables_ch.AutoSize = true;
+            this.all_tables_ch.Location = new System.Drawing.Point(14, 58);
+            this.all_tables_ch.Name = "all_tables_ch";
+            this.all_tables_ch.Size = new System.Drawing.Size(104, 17);
+            this.all_tables_ch.TabIndex = 24;
+            this.all_tables_ch.Text = "Všechny tabulky";
+            this.all_tables_ch.UseVisualStyleBackColor = true;
+            this.all_tables_ch.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(504, 437);
+            this.ClientSize = new System.Drawing.Size(604, 355);
+            this.Controls.Add(this.tabs);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Main";
             this.ShowIcon = false;
             this.Text = "RDB";
+            this.tabs.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabs;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ComboBox tables_cb;
+        private System.Windows.Forms.Button insert_bt;
+        private System.Windows.Forms.ListView preview;
+        private System.Windows.Forms.Button soubor_in_bt;
+        private System.Windows.Forms.TextBox cesta_in_tb;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.CheckBox all_tables_ch;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- RDB.UI/Forms/Main.cs
+++ RDB.UI/Forms/Main.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace RDB.UI.Forms
 {
@@ -26,8 +28,168 @@ namespace RDB.UI.Forms
             defaultContext = new DefaultContext();
 
             InitializeComponent();
+            GetScheme();
+            insert_bt.Enabled = false;
         }
 
         #endregion
+        private void GetScheme() //Získání názvu tabulek pro výběr
+        {
+            defaultContext.Database.Connection.Open();
+            DataTable dt = defaultContext.Database.Connection.GetSchema("Tables");
+            List<string> tables = new List<string>();
+            tables_cb.Items.Clear();
+            if (!all_tables)
+            {
+                foreach (DataRow row in dt.Rows)
+                {
+                    string tablename = (string)row[2];
+                    tables.Add(tablename);
+                    tables_cb.Items.Add(tablename);
+                }
+            }
+            defaultContext.Database.Connection.Close();
+        }
+
+        private bool all_tables = false;
+        string filePath = "";
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)   //Vyběr jedné tabulky nebo v souboru jsou všechny
+        {
+            if (all_tables_ch.Checked)
+            {
+                tables_cb.Enabled = false;
+                all_tables = true;
+            }
+            else
+            {
+                tables_cb.Enabled = true;
+                all_tables = false;
+            }
+        }
+
+        private void soubor_in_bt_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            //filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                //openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Textové soubory (*.txt)|*.txt|csv soubory (*.csv)|*.csv|xsl soubory (*.xsl)|*.xsl|Všechny soubory (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    cesta_in_tb.Text = filePath;
+                    //Read the contents of the file into a stream
+                    //var fileStream;
+                    try
+                    {
+                        var fileStream = openFileDialog.OpenFile();
+                        using (StreamReader reader = new StreamReader(fileStream, Encoding.Default))
+                        {
+                            bool prvni = true;
+                            //fileContent = reader.ReadToEnd();
+                            preview.View = View.Details;
+
+                            for (int i = 0; i < 10; i++)
+                            {
+                                try
+                                {
+                                    var line = reader.ReadLine();
+                                    if (line != null)
+                                    {
+                                        string[] values = line.Split(';');
+                                        string[] arr = new string[values.Length];
+                                        //var items = preview.Items;
+                                        //ListViewItem lvi1 = new ListViewItem();
+                                        ListViewItem item1 = new ListViewItem();
+                                        if (prvni)
+                                        {
+                                            for (int j = 0; j < values.Length; j++)
+                                            {
+                                                preview.Columns.Add("Sloupec " + j);
+                                                prvni = false;
+                                            }
+                                        }
+                                        preview.Items.Add(new ListViewItem(values));
+                                        //preview.Items.Add(values);
+                                    }
+                                }
+                                catch { }
+
+                            }
+                            insert_bt.Enabled = true;
+                            preview.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                            preview.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("SOubor nemohl být otevřen, může být otevřen v jiné aplikaci.");
+                    }
+                }
+            }
+        }
+
+        private void insert_bt_Click(object sender, EventArgs e) 
+        {
+            if (filePath.Length > 0 && ((tables_cb.Text.Length > 0 && !all_tables) || all_tables))
+            {
+                string tabulka = tables_cb.Text;
+                List<string> sloupce_list = new List<string>();
+                try                                                 //získání názvů sloupců pro vkládání dat
+                {
+                    defaultContext.Database.Connection.Open();
+                    String[] columnRestrictions = new String[4];
+                    columnRestrictions[2] = tabulka;
+                    DataTable sloupce = defaultContext.Database.Connection.GetSchema("Columns", columnRestrictions);
+                    foreach (DataRow row in sloupce.Rows)
+                    {
+                        string column = (string)row[3];
+                        sloupce_list.Add(column);
+                    }
+
+                    InsertColumns();    //volání vkládání
+                    defaultContext.Database.Connection.Close();
+                }
+                catch (SqlException exp)
+                {
+                    MessageBox.Show("Chyba:" + exp);
+                }
+            }
+        }
+        private void InsertColumns() //vložení hodnot ze souboru
+        {
+            int counter = 0;
+            string line;
+
+            System.IO.StreamReader file =
+            new System.IO.StreamReader(@filePath, Encoding.Default);
+
+            while ((line = file.ReadLine()) != null)
+            {
+                try
+                {
+                    /*
+                     *  Každá řádek -> dodělat 
+                     * 
+                     * 
+                     * 
+                     */
+                }
+                catch (SqlException e)
+                {
+                    MessageBox.Show("Chyba:" + e);
+                }
+                counter++;
+            }
+            MessageBox.Show("Hodnoty vloženy.");
+            file.Close();
+        }
     }
 }
